7f8fed33b1dcb6715de5dab257f05d2c9ed8cd03
Bonnie <bonniechiu527@gmail.com>
2025-07-11 09:59:46 +1000

    initial commit section 5.39 logic

---
+++ src/index.js
@@ -0,0 +1,117 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+
+
+const pizzaData = [
+  {
+    name: "Focaccia",
+    ingredients: "Bread with italian olive oil and rosemary",
+    price: 6,
+    photoName: "pizzas/focaccia.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Margherita",
+    ingredients: "Tomato and mozarella",
+    price: 10,
+    photoName: "pizzas/margherita.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Spinaci",
+    ingredients: "Tomato, mozarella, spinach, and ricotta cheese",
+    price: 12,
+    photoName: "pizzas/spinaci.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Funghi",
+    ingredients: "Tomato, mozarella, mushrooms, and onion",
+    price: 12,
+    photoName: "pizzas/funghi.jpg",
+    soldOut: false,
+  },
+  {
+    name: "Pizza Salamino",
+    ingredients: "Tomato, mozarella, and pepperoni",
+    price: 15,
+    photoName: "pizzas/salamino.jpg",
+    soldOut: true,
+  },
+  {
+    name: "Pizza Prosciutto",
+    ingredients: "Tomato, mozarella, ham, aragula, and burrata cheese",
+    price: 18,
+    photoName: "pizzas/prosciutto.jpg",
+    soldOut: false,
+  },
+];
+
+
+function App(){
+    return (
+        <div className="container"> 
+            <Header/>
+            <Footer/>
+            <Menu/>
+        </div>
+    );
+}
+
+function Header(){
+    return (
+        <header className="header">
+            <h1 >Fast React Pizza Co.</h1>
+        </header>
+    )
+}
+
+function Menu(){
+
+    return (
+        <main className="menu">
+            <h2>Our Menu</h2>
+            <Pizza name="pizza proscuitto" ingredients="Tomato, mozarella, ham, aragula, and burrata cheese"
+            photoName="pizzas/prosciutto.jpg" price={10} />
+
+            <Pizza name='Pizza Funghi' ingredients='Tomato, mozarella, mushrooms, and onion' 
+            photoName='pizzas/funghi.jpg'price={12}></Pizza>
+        </main>
+    )
+
+}
+
+function Footer(){
+    const hour = new Date().getHours()
+    const openHour = 12;
+    const closeHour = 22;
+
+    // if (hour >= openHour && hour <= closeHour ) alert('We are open for business!')
+    //     else alert('Sorry we are closed!');
+    // console.log(hour);
+    return (
+        <footer className="footer">{new Date().toLocaleDateString()}, We are currently open! </footer>
+    );
+}
+
+function Pizza(props){
+    return (
+        <div> 
+            <img src={props.photoName} alt={props.name}></img>
+            <div>
+                <h3>{props.name}</h3>
+                <p>{props.ingredients}</p>
+                <span>{props.price}</span>
+            </div>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
+
